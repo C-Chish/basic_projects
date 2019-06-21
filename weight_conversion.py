8588984085ce45fb8ed77324a17c4ddc98aefcf4
Carolina Falado <togykip@gmail.com>
2019-06-21 13:38:58 +1200

    fixed lower case option

--- weight_conversion.py
+++ weight_conversion.py
@@ -5,10 +5,10 @@ except ValueError:
     print("That's not a weight, please enter weight in Kgs or Lbs")
     exit()
 try:
-    kgs_or_lbs = input("Please pick (K) to convert to Kilos or (L) for pounds: ")
-    if kgs_or_lbs == str('k'):
+    kgs_or_lbs = input("Please pick (K) to convert to Kilos or (L) for pounds: ").upper()
+    if kgs_or_lbs == str('K'):
         print(f'you are {float(user_weight)*0.45}kgs!')
-    elif kgs_or_lbs == str('l'):
+    elif kgs_or_lbs == str('L'):
         print(f'you are {float(user_weight)*2.205} pounds!')
     else:
         print("That's not a valid answer, please enter pick (K) for kgs and (L) for pounds")
